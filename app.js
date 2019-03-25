5e87d1272131241d05078865d6e13a38b5a228a9
Rubal Bansal <rbansal.rbansal@gmail.com>
2019-03-26 00:17:32 +0530

    setup node and express with es6

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.status(200).send({
+        success: 'true',
+        message: 'Somethign is coming finally'
+    })
+});
+
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+});
